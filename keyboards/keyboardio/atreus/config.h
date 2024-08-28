a2abbc328ac1ae9d169fc89db735300795c394f3
Casey Sullivan <casey.sullivan@gmail.com>
2024-08-28 19:26:55 +0000

    atreus config

---
+++ keyboards/keyboardio/atreus/config.h
@@ -0,0 +1,30 @@
+/* Copyright (C) 2019, 2020  Keyboard.io, Inc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define TAPPING_TERM 150
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+// #define QUICK_TAP_TERM 0
+#define RETRO_TAPPING
+// #define RETRO_SHIFT 500
+// #define PERMISSIVE_HOLD
+// #define AUTO_SHIFT_REPEAT
+
+#define COMBO_TERM 25        // how quickly all combo keys must be pressed in succession to trigger
+#define COMBO_MUST_HOLD_MODS // if a combo triggers a modifier, only trigger when the combo is held
+#define COMBO_HOLD_TERM 175  // how long at least one of the combo keys must be held to trigger

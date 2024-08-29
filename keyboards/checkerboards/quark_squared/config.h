a73a79fb32816fbcebe2954dcd28ce664059fa37
Casey Sullivan <casey.sullivan@gmail.com>
2024-08-29 12:26:52 +0000

    quark but combos not working

---
+++ keyboards/checkerboards/quark_squared/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define TAPPING_TERM 150
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+// #define QUICK_TAP_TERM 0
+#define RETRO_TAPPING
+// #define RETRO_SHIFT 500
+// #define PERMISSIVE_HOLD
+// #define AUTO_SHIFT_REPEAT
+
+#define COMBO_TERM 25        // how quickly all combo keys must be pressed in succession to trigger
+#define COMBO_MUST_HOLD_MODS // if a combo triggers a modifier, only trigger when the combo is held
+#define COMBO_HOLD_TERM 175  // how long at least one of the combo keys must be held to trigger
--- keyboards/checkerboards/quark_squared/rules.mk
+++ keyboards/checkerboards/quark_squared/rules.mk
@@ -1,3 +1,6 @@
 # Disable unsupported hardware
 AUDIO_SUPPORTED = no
 BACKLIGHT_SUPPORTED = no
+
+# COMBO_ENABLE = yes
+# INTROSPECTION_KEYMAP_C = Combos.c

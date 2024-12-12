14aea498b493047fdbc886934e10e89e8f37db75
Casey Sullivan <casey@widewail.com>
2024-12-12 15:38:57 -0500

    planck tuning

--- keyboards/planck/rev6_drop/config.h
+++ keyboards/planck/rev6_drop/config.h
@@ -53,10 +53,10 @@
 #define TAPPING_TERM 150
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-// #define QUICK_TAP_TERM 0
+#define QUICK_TAP_TERM 0
 #define RETRO_TAPPING
 // #define RETRO_SHIFT 500
-// #define PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD
 // #define AUTO_SHIFT_REPEAT
 
 #define COMBO_TERM 25        // how quickly all combo keys must be pressed in succession to trigger

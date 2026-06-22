99a2a4d094e4a152c5fb11bc7a7e30db33290694
Casey Sullivan <casey.sullivan@gmail.com>
2026-06-21 20:21:11 -0400

    ibid

--- keyboards/boardsource/unicorne/config.h
+++ keyboards/boardsource/unicorne/config.h
@@ -43,13 +43,13 @@
 // #define MODS_TO_NEUTRALIZE { MOD_BIT(KC_LEFT_ALT), MOD_BIT(KC_LEFT_GUI), MOD_BIT(KC_RIGHT_GUI), MOD_BIT(KC_LEFT_CTRL)|MOD_BIT(KC_LEFT_SHIFT) }
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 150
+#define TAPPING_TERM 175
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-// #define QUICK_TAP_TERM 0
+#define QUICK_TAP_TERM 0
 #define RETRO_TAPPING
-// #define RETRO_SHIFT 500
-// #define PERMISSIVE_HOLD
+#define RETRO_SHIFT 500
+#define PERMISSIVE_HOLD
 // #define AUTO_SHIFT_REPEAT
 
 #define COMBO_TERM 25        // how quickly all combo keys must be pressed in succession to trigger
